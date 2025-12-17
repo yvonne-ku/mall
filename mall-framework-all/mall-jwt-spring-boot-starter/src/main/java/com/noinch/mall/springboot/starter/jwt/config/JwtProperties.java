3c88fc8cfb0eed622bed5050c1907defa4a2efcf
Yvonne Ku <13387301520@163.com>
2025-12-17 20:05:31 +0800

    1) Gateway Module，2) Token Filter

---
+++ mall-framework-all/mall-jwt-spring-boot-starter/src/main/java/com/noinch/mall/springboot/starter/jwt/config/JwtProperties.java
@@ -0,0 +1,37 @@
+package com.noinch.mall.springboot.starter.jwt.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * JWT配置属性
+ */
+@Data
+@ConfigurationProperties(prefix = "mall.jwt")
+public class JwtProperties {
+
+    /**
+     * JWT秘钥
+     */
+    private String secret = "";
+
+    /**
+     * 签发者
+     */
+    private String iss = "";
+
+    /**
+     * 算法
+     */
+    private String alg = "HS256";
+
+    /**
+     * 过期时间（秒）
+     */
+    private int expire = 3600;
+
+    /**
+     * 主题
+     */
+    private String subject = "login";
+}
