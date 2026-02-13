669c08519ab7447110c1cd8d6470d76d67dde167
Yvonne Ku <13387301520@163.com>
2026-02-13 18:12:28 +0800

    .sql

--- resources/00_mall-db.sql
+++ resources/00_mall-db.sql
@@ -1,4 +1,4 @@
--- 建库脚本
+-- PROD
 
 -- 基础数据数据库
 CREATE
@@ -31,3 +31,37 @@ DATABASE IF NOT EXISTS mall_product;
 -- BFF聚合层数据库
 CREATE
 DATABASE IF NOT EXISTS mall_bff;
+
+-- DEV
+
+-- 基础数据数据库
+CREATE
+DATABASE IF NOT EXISTS mall_basic_data_dev;
+
+-- 购物车数据库
+CREATE
+DATABASE IF NOT EXISTS mall_cart_dev;
+
+-- 用户数据库
+CREATE
+DATABASE IF NOT EXISTS mall_customer_dev;
+
+-- 消息数据库
+CREATE
+DATABASE IF NOT EXISTS mall_message_dev;
+
+-- 订单数据库
+CREATE
+DATABASE IF NOT EXISTS mall_order_dev;
+
+-- 支付数据库
+CREATE
+DATABASE IF NOT EXISTS mall_pay_dev;
+
+-- 商品数据库
+CREATE
+DATABASE IF NOT EXISTS mall_product_dev;
+
+-- BFF聚合层数据库
+CREATE
+DATABASE IF NOT EXISTS mall_bff_dev;
